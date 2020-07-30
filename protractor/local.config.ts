4e746d22f1fbe724f79bee6e321e889d94a4e4e3
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2020-07-30 09:42:36 -0500

    improve-test

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,7 +4,7 @@ export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  onPrepare: () => {
+  onPrepare: () => { 
     browser.ignoreSynchronization = true;
   }
 };
